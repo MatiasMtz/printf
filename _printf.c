6d80e819a7803edafb26403c1c10ff42fca4b7fe
MatiasMtz <4442@holbertonschool.com>
2022-03-14 17:36:16 +0000

    refresh

--- _printf.c
+++ _printf.c
@@ -14,31 +14,37 @@ print my_print[] = {
 	{"b", print_binary}, {"R", print_rot13}, {"%", print_p}, {NULL, NULL},
 };
 
-	unsigned int count = 0, flags = 0, length = 0;
+	unsigned int count = 0, flags = 0, length = 0, s = 0;
 	va_list arg;
 
 	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
 	while (format != NULL && format[count] != '\0')
 	{
 		flags = 0;
 		if (format[count] == '%')
 		{
-			while (flags < 10)
+			s = 0;
+			if (format[count + 1] == '\0')
+				return (-1);
+			while (my_print[flags].str)
 			{
-				if (my_print[flags].str[0] == format[count + 1])
+				if (my_print[count].pr[0] == format[i + 1])
 				{
-				length += (my_print[flags].f(arg)) - 2;
-				break;
+					length += (my_print[flags].f(arg)) - 2;
+					count++;
+					s = 1;
+					break;
 				}
-				flags++;
+				count++;
 			}
-			count++;
+			if (s == 0)
+				_putchar(format[i]);
 		}
 		else
-		{
 			_putchar(format[count]);
-		}
-		count++;
+	count++;
 	}
 	va_end(arg);
 	return (count);
