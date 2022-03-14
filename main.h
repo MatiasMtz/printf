35edb737d5d52a616cf0daa50f6e9943a3b6303b
MatiasMtz <4442@holbertonschool.com>
2022-03-14 16:18:46 +0000

    refresh

--- main.h
+++ main.h
@@ -13,6 +13,7 @@ typedef struct prt
 	char *str;
 	int (*f)(va_list args);
 } print;
+int _strlen(char *s);
 int print_int(va_list args);
 int print_char(va_list args);
 int print_string(va_list args);
