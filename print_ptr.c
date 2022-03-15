aace86dbd69d7b2183691055e3df7f3cf1ac23f8
ezesilva95 <4381@holbertonschool.com>
2022-03-14 18:50:33 -0700

    refresh

--- print_ptr.c
+++ print_ptr.c
@@ -6,19 +6,19 @@
  */
 int print_ptr(va_list args)
 {
-	void *p;
-	long int a;
-	int b;
+        void *p;
+        long int a;
+        int b;
 
-	p = va_arg(args, void *);
-	if (p == NULL)
-	{
-		_printf("(nil)");
-		return (6);
-	}
-	a = (unsigned long int)p;
-	_putchar('0');
-	_putchar('x');
-	b = _printf("%.", a);
-	return (b + 2);
+        p = va_arg(args, void *);
+        if (p == NULL)
+        {
+                _printf("(nil)");
+                return (6);
+        }
+        a = (unsigned long int)p;
+        _putchar('0');
+        _putchar('x');
+        b = _printf("%.", a);
+        return (b + 2);
 }
